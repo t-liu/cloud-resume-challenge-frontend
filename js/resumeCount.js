e2fdc07afc76df5b6b44c2d08d62a4dd091d1dd6
t-liu <thomas.s.liu@gmail.com>
2025-10-13 10:27:26 -0400

    update to get previous last viewed date

--- js/resumeCount.js
+++ js/resumeCount.js
@@ -8,10 +8,10 @@ function getVisitorCount() {
         .then(data => {
             console.log(data)
             let visitorCount = data.count
-            let lastViewedDate = new Date(data.lastViewed)
+            let previousLastViewedDate = new Date(data.previousLastViewed)
 
             document.getElementById('visitorCount').innerHTML = visitorCount;
-            document.getElementById('lastViewedDate').innerHTML = lastViewedDate.toLocaleString([], { hour12: true});
+            document.getElementById('lastViewedDate').innerHTML = previousLastViewedDate.toLocaleString([], { hour12: true});
         })
         .catch(error => console.warn(error))
 }
